c3cf57454de778dfe72ebe309d04e320c66d622d
sherif saad <ebinsaad@sherifs-MBP.cgocable.net>
2023-02-14 11:36:46 -0500

    modified app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Jenkins, Flask, and Docker at Git 9</h2>'
+    return '<h1>Hello from Jenkins, Flask, and Docker at Git 10</h1>'
 
 
 if __name__ == "__main__":
