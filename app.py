f830e54a71ae8b046d7639ffdd8c24b1760197ed
sherif saad <ebinsaad@sherifs-MBP.cgocable.net>
2023-02-13 23:44:59 -0500

    edit 2 app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker at Git 0</h2>'
+    return '<h1>Hello from Flask & Docker at Git 1</h2>'
 
 
 if __name__ == "__main__":
